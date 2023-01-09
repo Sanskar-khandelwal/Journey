f4b250b0c968eb37418cc93e1a5d00915bf7594f
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2023-01-09 22:01:13 +0530

    changing the project and restarting it, almost cleaned the server and deleted and again installed the client

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 // Dependencies import
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
 
 //local imports
 import postRoutes from "./routes/posts.js";
@@ -10,20 +8,8 @@ import postRoutes from "./routes/posts.js";
 //server code
 const app = express();
 
-app.use("/posts", postRoutes);
 
-app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
-app.use(cors());
-
-//Mongodb Atlas
-const CONNECTION_URL =
-  "mongodb+srv://Sam:mernFirstProject123@cluster0.iwhh4bw.mongodb.net/?retryWrites=true&w=majority";
-
-const PORT = process.env.PORT || 5000;
-
-mongoose
-  .connect(CONNECTION_URL)
-  .then(() => app.listen(PORT, console.log(`server running on port ${PORT}`)))
-  .catch((err) => console.log(err));
+app.listen(5000, () => {
+console.log("Server is listening on port number 5000")
+})
