c5243f357f5dafe9e08b61398dafe7261fa469be
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2023-01-12 16:23:56 +0530

    connected it with backend and also fetched the title

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,27 @@
-export default function Home(){
-    return (
-        <div className="home">
-            
-        </div>
-    )
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [achivements, setAchivements] = useState("");
+
+  useEffect(() => {
+    const fetchAchivements = async () => {
+      const response = await fetch("http://localhost:5000/api/achievements");
+      const json = await response.json();
+      if (response.ok) {
+        setAchivements(json);
+      }
+    };
+    fetchAchivements();
+  }, []);
+  console.log(achivements);
+  return (
+    <div className="home">
+      <div className="achivements">
+        {achivements &&
+          achivements.map((achivement) => (
+            <p key={achivement._id}>{achivement.title}</p>
+          ))}
+      </div>
+    </div>
+  );
 }
