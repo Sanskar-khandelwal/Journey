b09d6e2ab734d2e706fa16829c8b1ee5b160301d
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2023-01-13 22:39:30 +0530

    prettier did little styling "

--- client/src/components/AchivementsDetails.js
+++ client/src/components/AchivementsDetails.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const achivementDetails = ({achivement}) => {
-    return (
-        <div className="achivement-details">
-            <h4>{achivement.title}</h4>
-            <p><strong>disc</strong>{achivement.disc}</p>
-            <p><strong>disc</strong>{achivement.location}</p>
-            <p>{achivement.createdAt}</p>
-        </div>
-    )
-}
+const achivementDetails = ({ achivement }) => {
+  return (
+    <div className="achivement-details">
+      <h4>{achivement.title}</h4>
+      <p>
+        <strong>disc</strong>
+        {achivement.disc}
+      </p>
+      <p>
+        <strong>disc</strong>
+        {achivement.location}
+      </p>
+      <p>{achivement.createdAt}</p>
+    </div>
+  );
+};
 
 export default achivementDetails;
