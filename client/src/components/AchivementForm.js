2a660cf1a14af973110616dc7343cd737bb931ea
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2023-01-27 17:57:39 +0530

    installed axios and changed the api call from fetch to axios

--- client/src/components/AchivementForm.js
+++ client/src/components/AchivementForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAchivementsContext } from "../hooks/useAchivementsContext";
+import axios from "axios";
 
 export default function AchivementForm() {
   const { dispatch } = useAchivementsContext();
@@ -18,9 +19,10 @@ export default function AchivementForm() {
       [e.target.name]: e.target.value,
     }));
   }
-  
-  function handlePhoto(e){
-    setForm({...form, photo: e.target.files[0]});
+
+  function handlePhoto(e) {
+    setForm({ ...form, photo: e.target.files[0] });
+    console.log(form.photo);
   }
 
   const handleSubmit = async (e) => {
@@ -28,39 +30,57 @@ export default function AchivementForm() {
     // const { title, disc, location } = form;
     // const achivement = { title, disc, location };
     const formData = new FormData();
-    formData.append('title', form.title);
-    formData.append('disc', form.disc);
-    formData.append('location', form.location);
-    formData.append('photo', form.photo);
+    formData.append("title", form.title);
+    formData.append("disc", form.disc);
+    formData.append("location", form.location);
+    formData.append("photo", form.photo);
 
-    //using the fetch api to send the post request
-    const response = await fetch("http://localhost:5000/api/achievements", {
-      method: "POST",
-      // body: JSON.stringify(achivement),
-      formData,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const json = await response.json();
-
-    if (!response.ok) {
-      setError(json.error);
-    }
-    if (response.ok) {
-      setError(null);
-      console.log("new Workout error", json);
-      setForm({
-        title: "",
-        disc: "",
-        location: "",
+    //using axios to post
+    axios
+      .post("http://localhost:5000/api/achievements", formData)
+      .then((res) => {
+        setError(null);
+        console.log("new Workout", res.data);
+        setForm({
+          title: "",
+          disc: "",
+          location: "",
+        });
+        dispatch({ type: "CREATE_ACHIVEMENT", payload: res.data });
+      })
+      .catch((err) => {
+        console.log("new workout error");
+        console.log(err);
       });
-      dispatch({ type: "CREATE_ACHIVEMENT", payload: json });
-    }
   };
 
+  //using the fetch api to send the post request
+  //   const response = await fetch("http://localhost:5000/api/achievements", {
+  //     method: "POST",
+  //     body: JSON.stringify(achivement),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
+  //   const json = await response.json();
+
+  //   if (!response.ok) {
+  //     setError(json.error);
+  //   }
+  //   if (response.ok) {
+  //     setError(null);
+  //     console.log("new Workout error", json);
+  //     setForm({
+  //       title: "",
+  //       disc: "",
+  //       location: "",
+  //     });
+  //     dispatch({ type: "CREATE_ACHIVEMENT", payload: json });
+  //   }
+  // };
+
   return (
-    <form  onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit}>
       <h3>Add a new Achievement</h3>
       <label htmlFor="title">Achievement Title </label>
       <input
@@ -80,11 +100,11 @@ export default function AchivementForm() {
         value={form.location}
         onChange={handleForm}
       />
-      <input 
-      type = "file"
-      accept = ".png, .jpg, .jpeg"
-      name = "photo"
-      onChange = {handlePhoto}
+      <input
+        type="file"
+        accept=".png, .jpg, .jpeg"
+        name="photo"
+        onChange={handlePhoto}
       />
 
       <button>Add Achievements</button>
