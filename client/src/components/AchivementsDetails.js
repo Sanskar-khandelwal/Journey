eb1e51d93fd4fa621d172164be00ab12eaddce5e
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2023-02-02 02:30:47 +0530

    did some styling to the details component now it is looking much better

--- client/src/components/AchivementsDetails.js
+++ client/src/components/AchivementsDetails.js
@@ -1,6 +1,9 @@
+//react icons
+import { RiDeleteBin6Line } from "react-icons/ri";
 import React from "react";
 import { useAchivementsContext } from "../hooks/useAchivementsContext";
 const AchivementDetails = ({ achivement }) => {
+  const date = new Date(achivement.createdAt);
   const { dispatch } = useAchivementsContext();
 
   const handleClick = async () => {
@@ -18,24 +21,25 @@ const AchivementDetails = ({ achivement }) => {
     }
   };
   return (
-    <div>
-      <div className="h-52 w-4/5 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-3xl mx-auto py-6 px-4">
+    <div className=" w-4/5 md:w-3/5 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-3xl mx-auto py-6 px-14 m-10 text-white">
+      <div className="flex justify-between align-middle">
         <div>
-          <h4>{achivement.title}</h4>
-          <p>
-            <strong>disc :</strong>
-            {achivement.disc}
-          </p>
-          <p>
-            <strong>location: </strong>
-            {achivement.location}
-          </p>
+          <h4 className="text-3xl">{achivement.title}</h4>
+          <p className="text-xl mt-4">{achivement.disc}</p>
+          <p className="text-lg mt-2">{achivement.location}</p>
         </div>
-
-        <img src={`http://localhost:5000/${achivement.photo}`} />
-        {console.log(`http://localhost:5000/${achivement.photo}`)}
-        <p>{achivement.createdAt}</p>
-        <span onClick={handleClick}>delete</span>
+        <img
+          alt="dynamic image set by the user"
+          src={`http://localhost:5000/${achivement.photo}`}
+          className="w-56 h-56 rounded-2xl object-cover overflow-hidden"
+        />
+      </div>
+      <div className="flex justify-between align-middle mt-5">
+        <p>{date.toDateString()}</p>
+        <RiDeleteBin6Line
+          className="cursor-pointer transition ease-in-out hover:scale-125"
+          onClick={handleClick}
+        />
       </div>
     </div>
   );
