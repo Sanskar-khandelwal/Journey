a7ce253815cb40dead5632b1cccc76223b12f653
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2023-02-01 23:26:12 +0530

    added the upload as static

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cors());
-app.use(express.static("uploads"));
+app.use('/uploads', express.static("uploads"));
 
 //create storage function
 const storage = multer.diskStorage({
