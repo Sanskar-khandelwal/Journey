a83c2e47b27f20928477e485022f64461efa34b1
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2023-02-02 23:48:52 +0530

    styled the navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header>
-      <div className="container">
+      <div className="container h-20 flex justify-center align-middle text-4xl font-medium mb-10 shadow-md my-10">
         <Link to="/">
           <h1>Achivements</h1>
         </Link>
